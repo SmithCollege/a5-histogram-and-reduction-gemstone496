2128c68e9036c619b8d45a68b88f83c1fabe5860
jade <lilianpalosky@gmail.com>
2024-12-16 13:01:30 -0500

    finish histogramming (fastest task ever)

--- histogram/cpu.cpp
+++ histogram/cpu.cpp
@@ -4,55 +4,59 @@
 #include <iostream>
 #include <math.h>
 
-#define SIZE 100
+#define SIZE 100000000
 #define RUNS 100
 
 // function to calculate the scan on GPU
-int histogram(int *in, int size, int (*op)(int, int)){
-  int out = in[0]; // begin. crash on empty inputs.
-
-  for ( int i = 1; i < size; i++ ) {
-    out = op(out, in[i]);
+void histogram(unsigned char *buffer,
+			long size,
+			unsigned int *histo){
+  for ( int i = 0; i < size; i++ ) {
+    unsigned char item = buffer[i];
+    histo[item] += 1;
   }
 
-  return out;
 }
 
-int* generateVals(int length) {
-  int* out = (int*) malloc ( length*sizeof(char) );
-
+void generateVals(unsigned char* vals,
+		  long length) {
   for (int i = 0; i < length; i++) {
-    out[i] = (i*i*i +1) % 256;
+    vals[i] = 'a'; // i could randomize this or i could choose not to
   }
 
-  return out;
 }
 
 int main() {
   std::cout << "\ncpu" << SIZE; // record the size of the run for data collection
 
   // allocate input and output arrays
-  int* in, out;
-  in = (int*) malloc(SIZE*sizeof(int));
+  unsigned char *buffer; unsigned int *histo;
+  buffer = (unsigned char*) malloc( SIZE * sizeof(char) );
+  histo = (unsigned int*) malloc ( pow(2, 8*sizeof(char)) * sizeof(int) ); // an int for every possible char
+
+  generateVals(buffer, SIZE);
   
   // run 100 times or whatever
   for (int i = 0; i < RUNS; i++) {
-    // initialize inputs
-    for (int j = 0; j < SIZE; j++) {
-      in[j] = 1;
+
+    for (int j = 0; j < pow(2, 8*sizeof(char)); j++) { // reset
+      histo[j] = 0;
     }
-    
+
+    // time it
     const auto start{std::chrono::steady_clock::now()};
-    out = reduce(in, SIZE, &sum); // calculate with sum
+    histogram(buffer, SIZE, histo); // calculate with sum
     const auto end{std::chrono::steady_clock::now()};
     const std::chrono::duration<double> elapsed{end - start};
     std::cout << "," << elapsed.count();
 
-    if (out != SIZE) { std::cerr << "OUT: " << out << "   EXP: " << SIZE << "\n"; }
+    // verify that every letter was indeed 'a' as supplied
+    if (histo['a'] != SIZE) { std::cerr << "OUT: " << histo['a'] << "   EXP: " << SIZE << "\n"; } // all of them are 'a's
   }
 
   // free mem
-  free(in);
+  free(buffer);
+  free(histo);
 
   return 0;
 }
